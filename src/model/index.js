f5b10b297f463a799354dc220dd1ed2116d9d58f
Zhian Chen <351211168@qq.com>
2021-02-25 22:58:14 +1100

    Change the mongodb url

--- src/model/index.js
+++ src/model/index.js
@@ -3,7 +3,7 @@
 const mongoose = require("mongoose");
 const UserModel = require("./User");
 
-mongoose.connect("mongodb://127.0.0.1:27017/test", {
+mongoose.connect("mongodb://127.0.0.1:27017/jr-12", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
